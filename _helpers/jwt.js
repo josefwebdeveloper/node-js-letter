e6f4b359cac0931fcd4c72dd2bdf4c9641286580
josephs <josef2011@gmail.com>
2020-04-27 18:22:10 +0300

    hjj

--- _helpers/jwt.js
+++ _helpers/jwt.js
@@ -11,6 +11,7 @@ function jwt() {
             // public routes that don't require authentication
             '/users/authenticate',
             '/users/register',
+            '/users',
             '/article/create',
             '/article',
             '/article/random'
